06c65524d5d67c809f33e265b57a116893e21d47
Robert Rees <robert.rees@guardian.co.uk>
2015-02-18 15:54:24 +0000

    Updates the build and test process

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-./node_modules/.bin/http-server --silent &
+export BROWSER_NAME=${BROWSER_NAME:=chrome}
+
+export TEST_SERVER_PORT=${TEST_SERVER_PORT:=8880}
+
+./node_modules/.bin/http-server -p $TEST_SERVER_PORT --silent &
+PID=$!
 node test/integration/runner
+TEST_RUNNER_EXIT=$?
+kill $PID
+
+if [ $TEST_RUNNER_EXIT == "0" ]; then
+    exit 0
+else
+    exit 1
+fi
+
