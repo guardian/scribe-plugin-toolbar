1ba8215fa72ecff815fb2613fd6bceed76f8813a
David Smith <bigdave.smith@gmail.com>
2015-02-23 10:18:14 -0700

    Add test case to validate command value being passed

--- test/integration/main.spec.js
+++ test/integration/main.spec.js
@@ -38,9 +38,23 @@ describe('toolbar plugin', function () {
       var vendorButton = window.document.createElement('button');
       vendorButton.innerText = 'Leave vendor alone!';
 
+      // Create buttons with command value
+      var fontButtonImpact = window.document.createElement('button');
+      fontButtonImpact.setAttribute('data-command-value', 'Impact');
+      fontButtonImpact.setAttribute('data-command-name', 'fontName');
+      fontButtonImpact.classList.add('command-value');
+      fontButtonImpact.innerText = 'Set font to Impact';
+      var fontButtonArial = window.document.createElement('button');
+      fontButtonArial.setAttribute('data-command-value', 'Arial');
+      fontButtonArial.setAttribute('data-command-name', 'fontName');
+      fontButtonArial.classList.add('command-value');
+      fontButtonArial.innerText = 'Set font to Arial';
+
       // Add them to the DOM
       toolbarDiv.appendChild(defaultButton);
       toolbarDiv.appendChild(vendorButton);
+      toolbarDiv.appendChild(fontButtonArial);
+      toolbarDiv.appendChild(fontButtonImpact);
       body.appendChild(toolbarDiv);
 
       require(['../../src/scribe-plugin-toolbar'], function (toolbarPlugin) {
@@ -68,6 +82,22 @@ describe('toolbar plugin', function () {
             // We have a vendor button, it shouldn't be disabled
             expect(button.disabled).to.not.be.ok;
           }
+        });
+      });
+    });
+  });
+
+  when('clicking a button with an associated command value', function() {
+    it('should pass through the command value', function() {
+      return driver.executeScript(function() {
+        var commandValueButtons = window.document.querySelectorAll('.scribe-toolbar button.command-value');
+        Array.prototype.forEach.call(commandValueButtons, function(button) {
+          // Click the button
+          button.click();
+
+          // Verify that the command was executed with the appropriate value
+          var expectedFont = button.dataset.commandValue;
+          expect(document.queryCommandValue('fontName')).to.be(expectedFont);
         });
       });
     });
