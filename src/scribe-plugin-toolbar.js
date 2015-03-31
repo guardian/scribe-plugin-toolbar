be7aee38d457afa236aab49411f88ab7d1f633e7
Ankit Ahuja <aahuja@coursera.org>
2015-03-31 00:02:30 -0700

    Add support for disabled tags for commands as data attribute

--- src/scribe-plugin-toolbar.js
+++ src/scribe-plugin-toolbar.js
@@ -1,3 +1,17 @@
+/**
+ * Plugin to attach functionality to the Scribe editor toolbar.
+ * Example markup of toolbar:
+ *
+ * <button data-command-name="bold"<strong>B</strong></button>
+ * <button data-command-name="italic"><em>I</em></button>
+ * <button data-command-name="code" data-disabled-tags="ol,ul,table">Code</button>
+ *
+ * Supported data attributes:
+ *
+ * data-command-name: Command name.
+ * data-command-value: Command value.
+ * data-disabled-tags: Tags for which the command is disabled.
+ */
 define(function () {
 
   'use strict';
@@ -27,37 +41,45 @@ define(function () {
            */
         });
 
-        // Keep the state of toolbar buttons in sync with the current selection.
-        // Unfortunately, there is no `selectionchange` event.
-        scribe.el.addEventListener('keyup', updateUi);
-        scribe.el.addEventListener('mouseup', updateUi);
-
-        scribe.el.addEventListener('focus', updateUi);
-        scribe.el.addEventListener('blur', updateUi);
-
-        // We also want to update the UI whenever the content changes. This
-        // could be when one of the toolbar buttons is actioned.
-        scribe.on('content-changed', updateUi);
-
-        function updateUi() {
+        function update() {
           // Look for a predefined command.
           var command = scribe.getCommand(button.dataset.commandName);
-
           var selection = new scribe.api.Selection();
+          var disabledTags = button.dataset.disabledTags ? button.dataset.disabledTags.split(',') : [];
+
+          var shouldDisable = selection.getContaining(function(node) {
+            var nodeName = node.nodeName.toLowerCase();
+            return (disabledTags.indexOf(nodeName) !== -1);
+          });
+
+          // see https://github.com/guardian/scribe/issues/208
+          var isListCommand = ['insertUnorderedList', 'insertOrderedList'].indexOf(command) !== -1;
 
           // TODO: Do we need to check for the selection?
-          if (selection.range && command.queryState(button.dataset.commandValue)) {
+          if (selection.range && !isListCommand && command.queryState(button.dataset.commandValue)) {
             button.classList.add('active');
           } else {
             button.classList.remove('active');
           }
 
-          if (selection.range && command.queryEnabled()) {
+          if (selection.range && command.queryEnabled() && !shouldDisable) {
             button.removeAttribute('disabled');
           } else {
             button.setAttribute('disabled', 'disabled');
           }
         }
+
+        // Keep the state of toolbar buttons in sync with the current selection.
+        // Unfortunately, there is no `selectionchange` event.
+        scribe.el.addEventListener('keyup', update);
+        scribe.el.addEventListener('mouseup', update);
+
+        scribe.el.addEventListener('focus', update);
+        scribe.el.addEventListener('blur', update);
+
+        // We also want to update the UI whenever the content changes. This
+        // could be when one of the toolbar buttons is actioned.
+        scribe.on('content-changed', update);
       });
     };
   };
